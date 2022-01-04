f99e5bee176ddb9c3613f0e699abb4cb7687b81a
Ahn Sang Hoon <ansh0324@gmail.com>
2022-01-04 16:01:17 +0900

    init: First Swift Package

---
+++ Sources/SwiftPackageManager-Example/SwiftPackageManager_Example.swift
@@ -0,0 +1,6 @@
+public struct SwiftPackageManager_Example {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftPackageManager-ExampleTests/SwiftPackageManager_ExampleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftPackageManager_Example
+
+final class SwiftPackageManager_ExampleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftPackageManager_Example().text, "Hello, World!")
+    }
+}
